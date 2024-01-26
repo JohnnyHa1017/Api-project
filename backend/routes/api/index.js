3a831105d75a0a0bf143b346636b742d39a20def
Johnny Ha <johnny.ha.github@gmail.com>
2024-01-25 16:16:40 -0800

    Add User Login Backend Endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
-const { restoreUser } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+const { restoreUser } = require("../../utils/auth.js");
 
 router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
+
+module.exports = router;
 
 /*
 router.post('/test', function(req, res) {
@@ -38,5 +50,3 @@ router.get(
     }
     );
     */
-
-    module.exports = router;
