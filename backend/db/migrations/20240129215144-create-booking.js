3b59cdb363d004921bc90c0785f8adde0b0d4f77
Johnny Ha <johnny.ha.github@gmail.com>
2024-01-30 10:38:55 -0800

    adding proper requirements

--- backend/db/migrations/20240129215144-create-booking.js
+++ backend/db/migrations/20240129215144-create-booking.js
@@ -1,5 +1,8 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
+
+const { Booking } = require('../models');
+
 let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;
