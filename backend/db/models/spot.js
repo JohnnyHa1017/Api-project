33a01daaed06e88954e8cb14f144d75063220031
Johnny Ha <johnny.ha.github@gmail.com>
2024-01-30 11:47:19 -0800

    adding requirements

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -1,27 +1,33 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
-  class Spot extends Model {
+    class Spot extends Model {
     static associate(models) {
-			Spot.belongsTo(models.User, { foreignKey: "ownerId", as: "Owner" });
+
+			Spot.belongsTo(models.User, {
+        foreignKey: "ownerId",
+        as: "Owner"
+      });
 
 			Spot.hasMany(models.Review, {
 				foreignKey: "spotId",
 				onDelete: "CASCADE",
 				hooks: true,
 			});
+
 			Spot.hasMany(models.SpotImage, {
 				foreignKey: "spotId",
 				onDelete: "CASCADE",
 				hooks: true,
 			});
+
 			Spot.hasMany(models.Booking, {
 				foreignKey: "spotId",
 				onDelete: "CASCADE",
 				hooks: true,
 			});
+
     }
   }
   Spot.init({
