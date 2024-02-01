6aa95a73f382d247c6024ff032e76a51d0dd00b8
Johnny Ha <johnny.ha.github@gmail.com>
2024-02-01 14:56:57 -0800

    added a review editor and delete option

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -7,6 +7,17 @@ const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
+const validateReview = [
+  check('review')
+    .isString()
+    .exists({ checkFalsy: true })
+    .withMessage('Review text is required'),
+  check('stars')
+    .isInt({ min: 1, max: 5 })
+    .withMessage('Stars must be an integer from 1 to 5'),
+  handleValidationErrors,
+];
+
 const fetchUserReviews = async (req, res, next) => {
   const allSpots = await Spot.findAll();
   const detailedSpot = await Promise.all(allSpots.map(async (spot) => {
@@ -117,6 +128,50 @@ router.post('/:reviewId/images', requireAuth, countAttachedImages, async (req, r
   });
 
   res.status(200).json(newImageAttached);
+});
+
+router.put('/:reviewId', requireAuth, validateReview, async (req, res) => {
+  const { reviewId } = req.params;
+  const { review, stars } = req.body;
+  const currentUser = req.user.id;
+try {
+  const reviews = await Review.findByPk(reviewId);
+  if (!reviews) {
+    return res.status(404).json({ message: "Review couldn't be found" });
+  };
+  if(reviews.userId !== currentUser){
+    return res.status(403).json({ message: "You are not authorized."});
+};
+  await reviews.update(req.body);
+
+  const updatedReview = await Review.findByPk(reviewId);
+
+  return res.status(200).json(updatedReview);
+  } catch (error) {
+    if (error instanceof Sequelize.ValidationError) {
+      const validationErrors = handleValidationErrors(error.errors);
+      return res.status(400).json({
+        message: 'Bad Request',
+        errors: validationErrors,
+      });
+    }
+  }
+});
+
+router.delete("/:reviewId", requireAuth, async (req, res) => {
+  const { reviewId } = req.params;
+  const currentUser = req.user.id;
+
+  const reviews = await Review.findByPk(reviewId);
+  if (!reviews) {
+    return res.status(404).json({ message: "Review couldn't be found" });
+  };
+  if(reviews.userId !== currentUser){
+    return res.status(403).json({ message: "You are not authorized."});
+};
+await reviews.destroy();
+
+return res.status(200).json({ message: "Successfully deleted" });
 });
 
 module.exports = router;
