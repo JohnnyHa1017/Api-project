a395bc42516ff126d10bb138bbc98c183169712c
Johnny Ha <johnny.ha.github@gmail.com>
2024-01-31 18:39:50 -0800

    created a post route for new and valid spots

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -3,6 +3,8 @@ const { Sequelize } = require("sequelize");
 const { Op } = require("sequelize");
 const { requireAuth } = require("../../utils/auth");
 const { Spot, Review, User, SpotImage, ReviewImage, Booking } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
 module.exports = router;
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { Sequelize } = require('sequelize');
 const { Op } = require('sequelize');
-const { requireAuth } = require("../../utils/auth");
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { Spot, Review, User, SpotImage, ReviewImage, Booking } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
 const fetchSpots = async (req, res, next) => {
@@ -46,6 +48,49 @@ const fetchSpots = async (req, res, next) => {
   next();
 };
 
+const validateSpots = [
+  check('address')
+    .isString()
+    .exists({ checkFalsy: true })
+    .withMessage("Street address is required"),
+  check('city')
+    .isString()
+    .exists({ checkFalsy: true })
+    .withMessage("City is required"),
+  check('state')
+    .isString()
+    .exists({ checkFalsy: true })
+    .withMessage("State is required"),
+  check('country')
+    .isString()
+    .exists({ checkFalsy: true })
+    .withMessage("Country is required"),
+  check('lat')
+    .isDecimal()
+    .exists({ checkFalsy: true })
+    .withMessage("Latitude must be within -90 and 90")
+    .isFloat({ min: -90, max: 90 }),
+  check('lng')
+    .isDecimal()
+    .exists({ checkFalsy: true })
+    .withMessage("Longitude must be within -180 and 180")
+    .isFloat({ min: -180, max: 180 }),
+  check('name')
+    .isString()
+    .exists({ checkFalsy: true })
+    .withMessage("Name must be less than 50 characters"),
+  check('description')
+    .isString()
+    .exists({ checkFalsy: true })
+    .withMessage("Description is required"),
+  check('price')
+    .isDecimal()
+    .exists({ checkFalsy: true })
+    .withMessage("Price per day must be a positive number")
+    .isFloat({ min: 0 }),
+  handleValidationErrors
+];
+
 router.get('/', fetchSpots, (req, res) => {
   const { detailedSpot } = req;
   res.status(200).json({ Spots: detailedSpot });
@@ -66,7 +111,6 @@ router.get('/current', requireAuth, fetchSpots, async (req, res) => {
 router.get('/:spotId', fetchSpots, async (req, res) => {
   const { spotId } = req.params;
   const { detailedSpot } = req;
-
     const spotById = detailedSpot.find((spot) => spot.id == spotId);
 
     if (!spotById) {
@@ -112,6 +156,30 @@ router.get('/:spotId', fetchSpots, async (req, res) => {
         Owner: relatedOwner,
       }
   return res.status(200).json(response);
+});
+
+router.post('/', requireAuth, validateSpots, async (req, res) => {
+  const userId = req.user.id;
+    const {
+      address,
+      city,
+      state,
+      country,
+      lat,
+      lng,
+      name,
+      description,
+      price,
+    } = req.body;
+
+    const newValidSpot = await Spot.create({
+      ...req.body,
+      ownerId: userId,
+    });
+
+    await setTokenCookie(res, newValidSpot);
+
+  return res.status(201).json(newValidSpot);
 });
 
 module.exports = router;
