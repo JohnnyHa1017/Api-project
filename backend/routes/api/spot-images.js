29a6493f63b9be5ef27151b2c903a81f75d13a1e
Johnny Ha <johnny.ha.github@gmail.com>
2024-02-01 15:26:49 -0800

    spotimages delete created

--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -7,5 +7,22 @@ const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
+router.delete("/:imageId", requireAuth, async (req, res) => {
+	const { imageId } = req.params;
+	const currentUser = req.user.id;
+
+	const spotImage = await SpotImage.findByPk(imageId);
+	if (!spotImage) {
+		return res.status(404).json({ message: "Spot Image couldn't be found" });
+	}
+	const spot = await Spot.findByPk(spotImage.spotId);
+
+	if (!spot || spot.ownerId !== currentUser) {
+		return res.status(403).json({ message: "Unauthorized access" });
+	}
+	await spotImage.destroy();
+
+	res.status(200).json({ message: "Successfully deleted" });
+});
 
 module.exports = router;
